f4634da940808af46945168d9aba1900634c7e02
Ritche Reluao <34097542+ritchereluao@users.noreply.github.com>
2021-08-02 17:47:13 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,64 @@
+import requests
+from twilio.rest import Client
+
+STOCK = "TSLA"
+COMPANY_NAME = "Tesla Inc"
+
+STOCK_ENDPOINT = "https://www.alphavantage.co/query"
+NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+
+STOCK_API_KEY = ""
+NEWS_API_KEY = ""
+TWILIO_SID = ""
+TWILIO_AUTH_TOKEN = ""
+
+
+stock_params = {
+    "function": "TIME_SERIES_DAILY",
+    "symbol": STOCK,
+    "apikey": STOCK_API_KEY,
+}
+
+stock_response = requests.get(url=STOCK_ENDPOINT, params=stock_params)
+stock_response.raise_for_status()
+data = stock_response.json()["Time Series (Daily)"]
+data_list = [value for key, value in data.items()]
+yesterday_data = data_list[0]
+yesterday_closing_price = yesterday_data["4. close"]
+# print(yesterday_closing_price)
+
+day_before_yesterday_data = data_list[1]
+day_before_yesterday_closing_price = day_before_yesterday_data["4. close"]
+# print(day_before_yesterday_closing_price)
+
+difference = float(yesterday_closing_price) - float(day_before_yesterday_closing_price)
+up_down = None
+if difference > 0:
+    up_down = "📈"
+else:
+    up_down = "📉"
+
+diff_percent = round((difference / float(yesterday_closing_price)) * 100)
+# print(diff_percent)
+
+if abs(diff_percent) > 0:  # Change to 5
+    news_params = {
+        "apiKey": NEWS_API_KEY,
+        "qInTitle": COMPANY_NAME,
+    }
+    news_response = requests.get(url=NEWS_ENDPOINT, params=news_params)
+    news_response.raise_for_status()
+    articles = news_response.json()["articles"]
+    three_articles = articles[:3]
+    # print(three_articles)
+
+    formatted_articles = [f"{STOCK}: {up_down}{diff_percent}%\nHeadline: {article['title']}. \nBrief: {article['description']}" for article in three_articles]
+
+    client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+
+    for article in formatted_articles:
+        message = client.messages.create(
+            body=article,
+            from_="+17474778782",
+            to="+6588924815"
+        )
